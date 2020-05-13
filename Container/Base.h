6efabd52f648a34f8acabff58cd9efe81bf44302
ryazantsev <ryazantsev.gleb@gmail.com>
2020-05-13 16:12:55 +0700

    init commit

---
+++ Container/Base.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Printable
+{
+public:
+	virtual void print(ostream& out) const = 0;
+};
+
+class Named : Printable
+{
+public:
+	Named(string name)
+	{
+		m_name = name;
+	}
+
+protected:
+	string m_name;
+};
